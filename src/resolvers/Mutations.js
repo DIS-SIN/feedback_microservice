af8ba49f183c37f277eacfa89b2a4af88fbfe8ad
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2019-10-23 16:28:23 -0400

    Feedback first draft of Micro Service

--- src/resolvers/Mutations.js
+++ src/resolvers/Mutations.js
@@ -3,7 +3,28 @@ const { UserInputError } = require("apollo-server");
 
 
 // Mutation functions get listed below
-
+ async function createApp(_, args, context, info){
+    return await context.prisma.mutation.createApp({
+         data:{
+             name: args.name,
+             type: args.type
+         }
+     }, info);
+ }
+ async function createFeedback(_, args, context, info){
+    return await context.prisma.mutation.createFeedback({
+         data:{
+             email: copyValueToObjectIfDefined(args.email),
+             comment: args.comment,
+             app:{
+                 connect:{
+                     id: args.appID
+                 }
+             },
+             created: await new Date().toString()
+         }
+     }, info);
+ }
 
 
 
@@ -11,5 +32,6 @@ const { UserInputError } = require("apollo-server");
 // Export the functions below to be included through client facing graphQL interface
 
 module.exports = {
-
+    createApp,
+    createFeedback
 };
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,11 +2,30 @@ const {copyValueToObjectIfDefined} = require("./helper/objectHelper");
 const { addFragmentToInfo } = require("graphql-binding");
 
 // Query functions get listed below
-
+async function applications(_, args, context, info){
+    return await context.prisma.query.apps({
+        where:{
+            id: copyValueToObjectIfDefined(args.appID)
+        },
+        skip: copyValueToObjectIfDefined(args.skip),
+        first: copyValueToObjectIfDefined(args.first)
+    }, info);
+}
+async function feedback(_, args, context, info){
+    return await context.prisma.query.feedbacks({
+        where:{
+            id: copyValueToObjectIfDefined(args.feedbackID),
+            app:{
+                id: copyValueToObjectIfDefined(args.appID)
+            }
+        }
+    }, info)
+}
 
 
 
 // Export Query functions for inclusion in client facing graphQL interface
 module.exports = {
-
+    applications,
+    feedback
 };
